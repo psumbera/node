92a3e835ef5323d9826419c7ce549c73f3cc532b
Petr Sumbera <sumbera@volny.cz>
2026-02-19 23:01:59 +0100

    build: add sparcv9 as supported target arch

--- configure.py
+++ configure.py
@@ -47,7 +47,8 @@ parser = argparse.ArgumentParser()
 valid_os = ('win', 'mac', 'solaris', 'freebsd', 'openbsd', 'linux',
             'android', 'aix', 'cloudabi', 'os400', 'ios', 'openharmony')
 valid_arch = ('arm', 'arm64', 'ia32', 'mips', 'mipsel', 'mips64el',
-              'ppc64', 'x64', 'x86', 'x86_64', 's390x', 'riscv64', 'loong64')
+              'ppc64', 'x64', 'x86', 'x86_64', 's390x', 'riscv64', 'loong64',
+              'sparcv9')
 valid_arm_float_abi = ('soft', 'softfp', 'hard')
 valid_arm_fpu = ('vfp', 'vfpv3', 'vfpv3-d16', 'neon')
 valid_mips_arch = ('loongson', 'r1', 'r2', 'r6', 'rx')
@@ -1634,6 +1635,7 @@ def host_arch_cc():
     '__s390x__'   : 's390x',
     '__riscv'     : 'riscv',
     '__loongarch64': 'loong64',
+    '__sparc__'   : 'sparcv9',
   }
 
   rtn = 'ia32' # default
